b25e757d9dd5543bbbdb38a78f6d80c06fa42cb5
Kaio-Matias <matdantas89@gmail.com>
2025-12-01 22:20:27 -0300

    Ajustes nas apis

--- usu-rios-pacientes-master/src/app/controllers/User/index.ts
+++ usu-rios-pacientes-master/src/app/controllers/User/index.ts
@@ -1,9 +1,26 @@
-export { default as CreateUser } from './CreateUser';
-export { default as DeleteUser } from './DeleteUser';
-export { default as GetUser } from './GetUser';
-export { default as LoginUser } from './LoginUser';
-export { default as ResetPassword } from './ResetPassword';
-export { default as SendResetPasswordCodeInEmail } from './SendResetPasswordCodeInEmail';
-export { default as UpdateUser } from './UpdateUser';
-export { default as VerifyLoginUser } from './VerifyLoginUser';
-export { default as VerifyPasswordCodeInEmail } from './VerifyPasswordCodeInEmail';
+// 1. Importação da Classe (Named Export)
+import { CreateUserController } from './CreateUser';
+
+// 2. Importação das Funções (Default Exports)
+import deleteUser from './DeleteUser';
+import getUser from './GetUser';
+import loginUser from './LoginUser';
+import resetPassword from './ResetPassword';
+import sendResetPasswordCodeInEmail from './SendResetPasswordCodeInEmail';
+import updateUser from './UpdateUser';
+import verifyLoginUser from './VerifyLoginUser';
+import verifyPasswordCodeInEmail from './VerifyPasswordCodeInEmail';
+
+// 3. Exportação unificada para as Rotas
+// Para a classe, instanciamos e pegamos o método handle.
+// Para as funções, exportamos diretamente.
+
+export const CreateUser = new CreateUserController().handle;
+export const DeleteUser = deleteUser;
+export const GetUser = getUser;
+export const LoginUser = loginUser;
+export const ResetPassword = resetPassword;
+export const SendResetPasswordCodeInEmail = sendResetPasswordCodeInEmail;
+export const UpdateUser = updateUser;
+export const VerifyLoginUser = verifyLoginUser;
+export const VerifyPasswordCodeInEmail = verifyPasswordCodeInEmail;
